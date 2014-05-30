0108c159426bee72b54b25c7767371049c2a79c9
Dieterich Lawson <dieterich.lawson@gmail.com>
2014-05-29 21:55:18 -0700

    Cluster scripts

---
+++ bin/launch_cluster.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd ~/dev/spark/ec2 
+./spark-ec2 -k spark -i ~/Downloads/spark.pem \
+                --instance-type=m3.large \
+                --slaves=1 \
+                --spark-version=61fe0d09d6a7873647820565065e066e544d55c9 \
+                --spark-git-repo=https://github.com/darabos/spark.git \
+                launch lassotest
